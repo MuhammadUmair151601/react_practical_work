74233b6e4992cc2c7b405f1a5e262729c1d3a5be
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-08-26 16:52:36 +0500

    comp app created

---
+++ react-comp/src/App.jsx
@@ -0,0 +1,6 @@
+function app() {
+ return <h1>(hello world)</h1>;
+}
+// this is functional component:
+// others are class based components but now mostly functional components are used.
+export default app;
