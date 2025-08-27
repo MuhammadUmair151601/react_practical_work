c1d2680b3eaa7a66d9b7d1947842b4a10c78e88a
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-08-27 16:49:21 +0500

    jsx components exported to one another

--- react-comp/src/App.jsx
+++
@@ -1,6 +0,0 @@
-function app() {
- return <h1>(hello world)</h1>;
-}
-// this is functional component:
-// others are class based components but now mostly functional components are used.
-export default app;
---
+++ src/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello()
+{ 
+  let Name = "Umair"
+  return <h1>This is me {Name} speaking!</h1>
+}
+
+export default Hello;
